db69ddc8d6fa58c2252f3a1365a3673e6a569ff8
CrisDeniz <cdgm.7star@gmail.com>
2022-10-17 14:14:39 -0300

    design changes

--- pages/posts/[id]/[name].tsx
+++ pages/posts/[id]/[name].tsx
@@ -66,6 +66,30 @@ export default function Details({data, data2}: {data: Poke , data2: { color : { 
     return color
 }
   
+  function newTypeColor(type : string | undefined) {
+    let newTypeColor
+
+    type === 'poison' ? newTypeColor = 'rgb(177, 93, 255)':
+    type === 'grass' ? newTypeColor = 'rgb(200, 255, 149)':
+    type === 'bug' ? newTypeColor = 'rgb(217, 255, 2)':
+    type === 'fighting' ? newTypeColor = 'rgb(134, 49, 0)':
+    type === 'electric' ? newTypeColor = 'rgb(255, 208, 0)':
+    type === 'dragon' ? newTypeColor = 'rgb(124, 76, 255)':
+    type === 'ghost' ? newTypeColor = 'rgb(66, 45, 126':
+    type === 'fire' ? newTypeColor = 'rgb(255, 115, 115)':
+    type === 'dark' ? newTypeColor = 'rgb(68, 25, 0)':
+    type === 'fairy' ? newTypeColor = 'rgb(247, 159, 255)':
+    type === 'ground' ? newTypeColor = 'rgb(241, 185, 0)':
+    type === 'ice' ? newTypeColor = 'rgb(0, 255, 255)':
+    type === 'normal' ? newTypeColor = 'rgb(221, 221, 221)':
+    type === 'steel' ? newTypeColor = 'rgb(161, 161, 161)':
+    type === 'flying' ? newTypeColor = 'rgb(108, 196, 255)':
+    type === 'water' ? newTypeColor = 'rgb(58, 110, 255)':
+    type === 'psychic' ? newTypeColor = 'rgb(165, 4, 165)':
+    newTypeColor = 'rgb(10, 10, 10)'
+    
+    return newTypeColor
+  } 
 
     return(
       
@@ -76,40 +100,40 @@ export default function Details({data, data2}: {data: Poke , data2: { color : { 
                </div>
                 <h2>{data?.name}</h2>
                 <div className={styles.types}>
-                  <span>{data?.types?.[0].type.name}</span>
+                  <span style={{backgroundColor: newTypeColor(data?.types?.[0].type.name)}}>{data?.types?.[0].type.name}</span>
                   
                   {data?.types?.[1]?.type.name  
-                    ?<span>{data?.types?.[1]?.type.name}</span>
+                    ?<span style={{backgroundColor: newTypeColor(data?.types?.[1].type.name)}} >{data?.types?.[1]?.type.name}</span>
                     :null
                   }
                 
                 </div>
                 <div className={styles.stats}>
-                   
+                   <p style={{textAlign: 'center', fontWeight: 'bold'}} >Stats:</p>
                    <div className={styles.lineOne}>
-                    <div>
+                    <div style={{ backgroundColor: 'rgb(255, 115, 115)'}}>
                       <p>Hp</p>
-                      <p>{data.stats[0].base_stat}</p>
+                      <p >{data.stats[0].base_stat}</p>
                     </div>
                     <div>
                       <p>Atk</p>
                      <p>{data.stats[1].base_stat}</p>
                     </div>
-                    <div>
+                    <div style={{backgroundColor: "rgb(0, 89, 255)"}}>
                       <p>Def</p>
                      <p>{data.stats[2].base_stat}</p>
                     </div>
                    </div>
                    <div className={styles.lineTwo}>
-                   <div>
+                   <div  style={{backgroundColor: "rgb(145, 203, 255)"}}>
                      <p>Spc.Atk</p>
                      <p>{data.stats[3].base_stat} </p>
                     </div>
-                    <div>
+                    <div style={{backgroundColor: "rgb(0, 124, 6)"}}>
                       <p>Spc.Def</p>
                      <p>{data.stats[4].base_stat}</p>
                     </div>
-                    <div>
+                    <div style={{backgroundColor: "rgb(211, 170, 248)"}}>
                     <p>Spd</p>
                      <p>{data.stats[5].base_stat}</p>
                     </div>
